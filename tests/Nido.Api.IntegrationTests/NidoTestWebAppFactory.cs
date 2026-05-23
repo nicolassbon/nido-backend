6817d6858f8e40794ba90aa7232883ab4e8f44bd
Julieta <julibernacchia@gmail.com>
2026-05-23 19:48:03 -0300

    fix nidotestwebappfactory.cs

--- tests/Nido.Api.IntegrationTests/NidoTestWebAppFactory.cs
+++ tests/Nido.Api.IntegrationTests/NidoTestWebAppFactory.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Nido.Infrastructure.Persistence;
@@ -46,7 +47,8 @@ public sealed class NidoTestWebAppFactory : WebApplicationFactory<Program>
             _connection.Open();
 
             services.AddDbContext<NidoDbContext>(options =>
-                options.UseSqlite(_connection));
+                options.UseSqlite(_connection)
+                       .ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning)));
 
             // Create the schema so controllers can query/insert without crashing.
             using var scope = services.BuildServiceProvider().CreateScope();
